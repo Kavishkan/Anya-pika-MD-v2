01ab6765863b8a52ee6943b09dee45e722405e19
Kavishkan <118058278+Kavishkan@users.noreply.github.com>
2023-01-11 06:41:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,9 +15,9 @@ actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of 
 global.headerText = "Anya by Pika" // text for header of menu //        
      
              //-------------👑Owner & vcard 👑----------\\             
-             global.ownernumber = ['918811074852']   // Owner number //
-             global.vcardowner = ['918811074852']   // vcard owner number //
-             global.ownername = "3.69_Pika"   // Owner name //
+             global.ownernumber = ['0717230664']   // Owner number //
+             global.vcardowner = ['0717230664']   // vcard owner number //
+             global.ownername = "3.69_kavishka    // Owner name //
              global.ytname = "YT: ONYXPIKA"   // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4"   // Github or insta name //
              global.location = "India, Assam, Dibrugarh"   // Location //
